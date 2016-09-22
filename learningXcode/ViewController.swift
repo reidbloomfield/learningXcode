d0228b1c57a1e0d2da4ade5f21934f8b66e607bc
Reid Bloomfield <reid@Reids-MacBook-Air.local>
2016-09-22 09:56:21 -0400

    added another note: git

--- learningXcode/ViewController.swift
+++ learningXcode/ViewController.swift
@@ -19,6 +19,11 @@ class ViewController: UIViewController {
     
     //Adding a second note here..........
     
+    
+    
+    
+    // this note was added on my laptop
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
